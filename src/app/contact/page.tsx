97c8c9aec40a818dec5de9a87bd138e0787c8ffe
Henrik Nordberg <hnordberg@apache.org>
2025-11-06 11:21:10 -0800

    Removed header

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
-import './style.css'
+import './style.css';
 
 const ContactPage = () => {
   return (
     <main>
-      <h1>Contact</h1>
       <div className="contact-container">
         <Image src='/img/henrik.avatar.2024.jpeg' width="100" height="100" alt="Photo of Henrik Nordberg" title="Photo of Henrik Nordberg" />
         <div className="contact-details">
