515c76381395ba899bc3fb7f3520b0ea361ba0fa
Henrik Nordberg <hnordberg@apache.org>
2025-11-06 10:56:43 -0800

    Added pic of Henrik

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+import './style.css'
+
 const ContactPage = () => {
   return (
     <main>
       <h1>Contact</h1>
-      <p>hnordberg@apache.org<br /><a href='https://linkedin.com/in/hnordberg'>linkedin.com/in/hnordberg</a> </p>
+      <div className="contact-container">
+        <Image src='/img/henrik.avatar.2024.jpeg' width="100" height="100" alt="Photo of Henrik Nordberg" title="Photo of Henrik Nordberg" />
+        <div className="contact-details">
+          hnordberg@apache.org<br />
+          <a href='https://linkedin.com/in/hnordberg'>linkedin.com/in/hnordberg</a>
+        </div>
+      </div>
     </main>
   );
 };
