e118f59a1d0e207683e4031247ff380c0ddb2483
Henrik Nordberg <hnordberg@apache.org>
2025-11-06 09:57:52 -0800

    Basic contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+const ContactPage = () => {
+  return (
+    <main>
+      <h1>Contact</h1>
+      <p>hnordberg@apache.org<br /><a href='https://linkedin.com/in/hnordberg'>linkedin.com/in/hnordberg</a> </p>
+    </main>
+  );
+};
+
+export default ContactPage;
