f0b10dc578fa2c8bf893a85d882ddea610fcb32d
Henrik Nordberg <hnordberg@apache.org>
2025-11-08 15:13:24 -0800

    Made the card styles more general and starting to reuse them.

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -1,26 +1,14 @@
-import fs from 'fs'
-import path from 'path'
-import './style.css'
-
-interface Technology {
-  title: string;
-  description: string;
-}
-
 const TechnologyPage = () => {
-  const techFilePath = path.join(process.cwd(), 'data/technology.json')
-  const techFileContent = fs.readFileSync(techFilePath, 'utf8')
-  const tech: Technology[] = JSON.parse(techFileContent);
 
   return (
     <main>
-      <section id="technology">
-        {tech.map((tech, index) => (
-          <div className="tech-card" key={index}>
-            <h2>{tech.title}</h2>
-            <p>{tech.description}</p>
+      <section className="card-grid">
+        <div className="card hidden">
+          <div className="card-title">.</div>
+          <div className="card-text">.
           </div>
-        ))}
+          <div className="card-subtitle pt-4">.</div>
+        </div>
       </section>
     </main>
   )
