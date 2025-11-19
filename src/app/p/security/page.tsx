672591a7e072411117fb3c0e27c0bf581f517c57
Henrik Nordberg <hnordberg@apache.org>
2025-11-19 11:39:56 -0800

    Wrote about XSS, CSRF, and SSRF

--- src/app/p/security/page.tsx
+++ src/app/p/security/page.tsx
@@ -6,7 +6,7 @@ const OWASPPage = () => {
         <section id="best-practices">
           <h2>Best Practices for Mitigation</h2>
           <p>
-            Don't <i>just</i> try to reason about what a hacker might do. The way to protect yourself is to look up how to mitigate for each vulnerability.
+            Don't <i>just</i> try to reason about what a hacker might do. The way to protect yourself is to look up how to mitigate each vulnerability.
             As an example, let's say you want to protect against path traversal vulnerabilities. You know you shouldn't allow '../' in paths.
             So you write code to remove '../' from paths. This is not enough as the hacker could pass '.../...//', which would yield '../' after
             removing '../'. Read more on the <a href="https://cwe.mitre.org/data/definitions/22.html">CWE-22</a> page. It is always
@@ -17,27 +17,27 @@ const OWASPPage = () => {
           <h1>Web Standards</h1>
           <h2>Content Security Policy (CSP)</h2>
           <p>
-            Content Security Policy (CSP) allows whitelisting of sources of content that are allowed to be loaded. Helps prevent XSS attacks.
+            Content Security Policy (CSP) allows whitelisting sources of content that are allowed to be loaded. Helps prevent XSS attacks.
             CSP supersedes <code>X-Frame-Options</code>.
           </p>
           <h2>Cross-Origin Resource Sharing (CORS)</h2>
           <p>
-            Cross-Origin Resource Sharing (CORS) allows whitelisting of origins (i.e., web sites) that are allowed to access a resource. By 
-            default the browser prevents scripts on the page from accessing resources on other domains. CORS allows you to configure
-            what other domains are allowed to access the resource. This is done by setting the Access-Control-Allow-Origin header. 
+            Cross-Origin Resource Sharing (CORS) allows whitelisting websites (origins) that are allowed to access a resource. By
+            default, the browser prevents scripts on the page from accessing resources on other domains. CORS allows you to configure
+            what other domains are allowed to access the resource. This is done by setting the Access-Control-Allow-Origin header.
             The flow looks like this:
           </p>
           <ol>
             <li>The browser sends a request with <code>Origin: https://example.com</code> to the server.</li>
-            <li>The server looks at the <code>Origin</code> header and checks its CORS policy. If it approves, the 
-            server sets the <code>Access-Control-Allow-Origin: https://example.com</code> header in the response.</li>
+            <li>The server looks at the <code>Origin</code> header and checks its CORS policy. If it approves, the
+              server sets the <code>Access-Control-Allow-Origin: https://example.com</code> header in the response.</li>
             <li>The browser looks at the <code>Access-Control-Allow-Origin</code> header and allows the request to proceed,
-            if its value matches its origin.</li>
+              if its value matches its origin.</li>
           </ol>
           <p>
-            For requests using methods other than GET, POST, or HEAD, or POSTs with a Content-Type other than application/x-www-form-urlencoded,
-            multipart/form-data, or text/plain, or if the request has a custom header, the browser will send a <i>preflight request</i> to the server. 
-            The preflight request will be an OPTIONS request with the following headers: Origin, Access-Control-Request-Method, 
+            For requests using methods other than GET, POST, or HEAD, or POST requests with a Content-Type other than application/x-www-form-urlencoded,
+            multipart/form-data, or text/plain, or if the request has a custom header, the browser will send a <i>preflight request</i> to the server.
+            The preflight request will be an OPTIONS request with the following headers: Origin, Access-Control-Request-Method,
             Access-Control-Request-Headers. If the server approves, it responds with 200 OK.
           </p>
           <h2>HTTP Strict Transport Security (HSTS)</h2>
@@ -51,10 +51,35 @@ const OWASPPage = () => {
           </p>
         </section>
 
+        <section id="three-acronyms">
+          <h1>XSS, CSRF, and SSRF</h1>
+          <h2>Comparison</h2>
+          <p>
+            XSS and CSRF are both client-side attacks, while SSRF is a server-side attack. XSS attacks are executed by injecting malicious
+            code into a web page of another user. It can be used to extract information from the browser's context (e.g., cookies, DOM).
+            With CSRF, the attacker crafts a malicious request (e.g., an HTML image tag, an auto-submitting form, or a link) and tricks an authenticated user into
+            loading it. Since the user is logged into the target application, their browser automatically includes their session cookies with the forged
+            request, which the trusted site then executes as a legitimate user action. SSRF attacks are executed by manipulating a request
+            such that, when executed, it causes the server to make a request to an unintended site.
+          </p>
+          <h2>XSS -- Cross-Site Scripting</h2>
+          <p>
+            XSS -- inject code into a web page of another user. Extract information from the browser's context (e.g., cookies, DOM).
+          </p>
+          <h2>CSRF -- Cross-Site Request Forgery</h2>
+          <p>
+            CSRF -- forge a request to a trusted site on behalf of an authenticated user. Cause authenticated actions to occur.
+          </p>
+          <h2>SSRF -- Server-Side Request Forgery</h2>
+          <p>
+            SSRF -- forge a request to an unintended site on behalf of an authenticated user.
+          </p>
+        </section>
+
         <h1>The OWASP Top 10</h1>
         <p className="intro">The <a href="https://owasp.org/Top10/2025/0x00_2025-Introduction/">OWASP Top 10</a> is a list of
-         the most common security vulnerabilities affecting web applications. For each category, OWASP lists the most common vulnerabilities,
-         which they number using the Common Weakness Enumeration (CWE). Examples are XSS, SQL injection, and CSRF.
+          the most common security vulnerabilities affecting web applications. For each category, OWASP lists the most common vulnerabilities,
+          which they number using the Common Weakness Enumeration (CWE). Examples are XSS, SQL injection, and CSRF.
         </p>
 
         <section id="a01-broken-access-control">
@@ -83,18 +108,16 @@ const OWASPPage = () => {
 
         <section id="a05-injection">
           <h2>A05:2025 - Injection</h2>
-          <p>
           <ul className="list">
-              <li>SQL injection</li>
-              <li>Cross-site scripting (XSS). This is where the attacker injects code into the web page.
-                Commonly exploited features include URL parameters, search fields, and forms.
-              </li>
-              <li>Cross-site request forgery (CSRF). </li>
-              <li>Directory traversal</li>
-              <li>File inclusion</li>
-              <li>Command injection</li>
-            </ul>
-          </p>
+            <li>SQL injection</li>
+            <li>Cross-site scripting (XSS). This is where the attacker injects code into the web page.
+              Commonly exploited features include URL parameters, search fields, and forms.
+            </li>
+            <li>Cross-site request forgery (CSRF). </li>
+            <li>Directory traversal</li>
+            <li>File inclusion</li>
+            <li>Command injection</li>
+          </ul>
         </section>
 
         <section id="a06-insecure-design">
