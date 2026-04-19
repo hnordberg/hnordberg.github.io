26404389295088e7b7d4ef006347aca4f08d89b4
Henrik Nordberg <hnordberg@apache.org>
2026-04-19 09:49:29 -0700

    Removed the Contents from the ML page as all boxes are visible most of the time.

--- src/app/ml/page.tsx
+++ src/app/ml/page.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link';
-import Contents from '../components/Contents'
 
 const BookIcon = () => (
   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477-4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
   </svg>
 );
 
@@ -32,17 +31,9 @@ const BulbIcon = () => (
 )
 
 const MLPage = () => {
-  const articles = [
-    { id: 'wiki', title: 'ML Wiki' },
-    { id: 'llm', title: 'LLM Papers' },
-    { id: 'interesting', title: 'Interesting Papers' },
-    { id: 'transformer', title: 'Transformer' },
-    { id: 'suggest-topic', title: 'Suggest a wiki topic' },
-  ]
 
   return (
     <main className="page-with-contents">
-      <Contents articles={articles} />
       
       <div className="flex-1 w-full min-w-0 mt-4 xl:mt-0">
         <header className="mb-10 space-y-3 px-2">
